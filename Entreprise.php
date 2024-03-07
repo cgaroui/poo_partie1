2f8d0da1363a9c620df004a1b52981a04fcb6cdd
chaima garoui <chaimagaroui2@gmail.com>
2024-03-07 11:00:38 +0100

    1st commit

---
+++ Entreprise.php
@@ -0,0 +1,87 @@
+
+<?php 
+
+class Entreprise{
+    private string $raisonSociale;
+    private DateTime $dateCreation;
+    private string $adresse;
+    private string $cp;
+    private string $ville;
+
+    public function __construct(string $raisonsociale, string $dateCreation, string $adresse, 
+    string $cp, string $ville) {
+        $this->raisonSociale = $raisonSociale;
+        $this->dateCreation = new DateTime ($dateCreation);
+        $this->adresse = $adresse;
+        $this->cp = $cp;
+        $this->ville = $ville; 
+    }
+
+
+    public function getRaisonSociale():string
+    {
+        return $this->raisonSociale;
+    }
+
+    
+    public function setRaisonSociale($raisonSociale)
+    {
+        $this->raisonSociale = $raisonSociale;
+
+        return $this;
+    }
+
+    public function getDateCreation(): DateTime
+    {
+        return $this->dateCreation;
+    }
+
+   
+    public function setDateCreation($dateCreation)
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+  
+    public function getAdresse(): string
+    {
+        return $this->adresse;
+    }
+
+    
+    public function setAdresse($adresse) 
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getCp(): string
+    {
+        return $this->cp;
+    }
+
+   
+    public function setCp($cp)
+    {
+        $this->cp = $cp;
+
+        return $this;
+    }
+
+  
+    public function getVille() : string
+    {
+        return $this->ville;
+    }
+
+   
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+}
